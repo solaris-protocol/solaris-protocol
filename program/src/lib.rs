1878eae78f9fc29cfa1fa6d87727f1b656508456
Kirill Madorin <madorinkv@gmail.com>
2021-06-07 23:21:59 +0300

    merge with cli branch of token-lending program

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -12,4 +12,4 @@ pub mod state;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("TokenLending1111111111111111111111111111111");
+solana_program::declare_id!("6h5geweHee42FbxZrYAcYJ8SGVAjG6sGow5dtzcKtrJw");
